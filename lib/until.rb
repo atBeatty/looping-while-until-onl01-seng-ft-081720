1be4dfa402152c1bf9e1c22921b569fa848db610
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-18 18:19:37 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -4,7 +4,7 @@ def using_until
   res_string = ''
   levitation_force = 6
   until levitation_force == 10
-    res_string +="Wingardium Leviosa\n"
+    res_string += "Wingardium Leviosa\n"
     levitation_force += 1
   end
 
