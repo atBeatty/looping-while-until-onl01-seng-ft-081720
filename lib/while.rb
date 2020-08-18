2a86860e5ca12b3346bbb4d35c0710f65d7dc1ea
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-18 18:20:44 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -4,8 +4,9 @@ def using_while
   #your code heredef using_until
   levitation_force = 6
   res_string = ''
-  while levitation_force == 10
+  while levitation_force <= 10
     res_string += "Wingardium Leviosa\n"
     levitation_force += 1
   end
+
 end
